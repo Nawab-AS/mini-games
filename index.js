99e68c16e9eed9979801fa39cbc09c77ff2446bf
Hamid Syed <59747708+Nawab-AS@users.noreply.github.com>
2025-12-03 23:10:32 +0000

    feat: added window dragging

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const { createApp, reactive, ref } = Vue;
 
-const message = ref('Welcome to Games-OS!');
 const windows = reactive([]);
 
 setTimeout(() => {
     createWindow({
         title: 'Game 1',
         url: 'https://example.com',
-        width: 800,
-        height: 600,
+        width: 300,
+        height: 300,
         x: 100,
         y: 100,
         minWidth: 400,
@@ -18,32 +17,68 @@ setTimeout(() => {
 
 
 
-
+// Opening/closing windows
 function createWindow(options={title:'', url:'', width:600, height:400, x:50, y:50, minWidth:300, minHeight:200}) {
     const id = Math.random().toString(36).substring(2, 9);
-    windows.push({ id, state: 'opening', ...options });
+    windows.push({ id, anim: 'opening', ...options });
     setTimeout(() => {
-        windows.find(w => w.id == id).state = 'open';
+        windows.find(w => w.id == id).anim = '';
     }, 300);
     return id;
 }
 
-function closeWindow(id) {
+function closeWindow(e, id) {
+    e.stopPropagation();
     const i = windows.findIndex(win => win.id === id);
     if (i == -1) return;
-
-    windows[i].state = 'closing';
+    windows[i].anim = 'closing';
     setTimeout(() => {
         windows.splice(i, 1);
     }, 300);
 }
 
+// Dragging windows
+let dragData = null;
+function startDrag(e, window) {
+    if (e.button !== 0) return; // left click
+    e.preventDefault();
+    e.stopPropagation();
+    dragData = {
+        window,
+        startX: e.clientX,
+        startY: e.clientY,
+        origX: window.x,
+        origY: window.y
+    };
+}
+
+document.addEventListener('mouseup', (e) => {
+    if (!dragData) return;
+    e.preventDefault();
+    e.stopPropagation();
+    dragData = null;
+});
+
+document.addEventListener('mousemove', (e) => {
+    if (!dragData) return;
+    e.preventDefault();
+    e.stopPropagation();
+    const dx = e.clientX - dragData.startX;
+    const dy = e.clientY - dragData.startY;
+    dragData.window.x = dragData.origX + dx;
+    dragData.window.y = dragData.origY + dy;
+});
+
+
+
+
+// mount Vue
 const app = createApp({
     setup() {
         return {
             windows,
-            message,
             closeWindow,
+            startDrag,
         }
     }
 });
